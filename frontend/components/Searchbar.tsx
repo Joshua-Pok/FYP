589d21f5783d8ecd44d98d41c33180431087a5b6
Joshua-Pok <official.joshuapok@gmail.com>
2025-08-22 11:16:02 +0800

    searchbar

---
+++ frontend/components/Searchbar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { TextInput } from "react-native-paper";
+export default function Searchbar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <TextInput
+      label="Search for Activities"
+      value={searchTerm}
+      onChangeText={(text) => setSearchTerm(text)}
+    />
+  );
+}
