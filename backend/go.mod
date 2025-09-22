0a9481d6604db33a44d62ca66a7daadd85fee198
Joshua-Pok <official.joshuapok@gmail.com>
2025-09-22 16:40:27 +0800

    cacheservice

--- backend/go.mod
+++ backend/go.mod
@@ -5,5 +5,11 @@ go 1.24.5
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.14.0
 	golang.org/x/crypto v0.41.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
--- backend/service/gorseService.go
+++ backend/service/gorseService.go
@@ -1,3 +1,63 @@
 package service
 
-func NewGorseService() *T
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type GorseService struct {
+	BaseURL string
+	Client  *http.Client
+}
+
+func NewGorseService(baseURL string) *GorseService {
+	return &GorseService{
+		BaseURL: baseURL,
+		Client: &http.Client{
+			Timeout: 5 * time.Second,
+		},
+	}
+}
+
+func (g *GorseService) GetRecommendations(userId string, limit int) ([]string, error) {
+	url := fmt.Sprintf("%s/api/recommend/user/%s?number=%d", g.BaseURL, userId, limit)
+
+	resp, err := g.Client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting to Gorse: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Gorse returned %d", resp.StatusCode)
+	}
+
+	var ids []string
+	if err := json.NewDecoder(resp.Body).Decode(&ids); err != nil {
+		return nil, fmt.Errorf("failed to decode Gorse reponse %w", err)
+	}
+	return ids, nil
+}
+
+func (g *GorseService) GetPopularActivities(limit int) ([]string, error) {
+	url := fmt.Sprintf("%s/api/item/popular?number=%d", g.BaseURL, limit)
+
+	resp, err := g.Client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Error retrieving popular items from gorse %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Gorse returned status %d", resp.StatusCode)
+	}
+
+	var ids []string
+	if err := json.NewDecoder(resp.Body).Decode(&ids); err != nil {
+		return nil, fmt.Errorf("failed to decode popular items from response")
+	}
+
+	return ids, nil
+}
